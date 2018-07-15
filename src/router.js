effa82e82dcf943fc70be427f638c4cc2ca7b242
zhangyongjie <yongjie_zhang@outlook.com>
2018-07-15 17:18:15 +0800

    完成slogn

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+import Slogan from "./views/Slogan.vue";
+import welcome from "./views/welcome.vue";
 
 Vue.use(Router);
 
@@ -9,13 +9,15 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "slogan",
+      component: Slogan,
+      meta:{title:'公益猫'}
     },
     {
-      path: "/about",
-      name: "about",
-      component: About
-    }
+      path: "/welcome",
+      name: "welcome",
+      component: welcome,
+      meta:{title:'加入公益猫'}
+    },
   ]
 });
---
+++ src/views/welcome.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main">
+    <div class="title">
+      <img src="../assets/register-title_06.jpg" width="70%">
+      <h2>注册账号</h2>
+    </div>
+    <div class="form">
+      <component :is="view"></component>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import register from '../components/welcome/register.vue';
+export default {
+  data() {
+    return {
+      view:'register'
+    }
+  },
+  components: {
+    register,
+  }
+}
+</script>
+
+<style scoped lang="less">
+.main{
+  .title{
+    text-align: center;
+    h2{
+      padding: 20px 0;
+      color: #fc8d59;
+    }
+  }
+  width: 30%;
+  height: 500px;
+  margin: 70px auto;
+  //box-shadow:0 0 10px 0 rgb(255, 192, 109);
+}
+</style>
